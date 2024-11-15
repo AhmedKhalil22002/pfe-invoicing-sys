ec4fb774ee225971422f6f3d66694830ffb2b9b0
Houssem Eddine Chibouni <153816533+zc-houssem@users.noreply.github.com>
2024-11-15 12:16:44 +0100

    style(Layout.tsx) - change font family

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,12 +4,14 @@ import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import { IMenuItem } from '@/components/layout/interfaces/MenuItem.interface';
 import { BreadcrumbContext, BreadcrumbRoute } from './BreadcrumbContext';
+import { Inter } from 'next/font/google';
 
 interface LayoutProps {
   children: React.ReactNode;
   className?: string;
   items: IMenuItem[];
 }
+const inter = Inter({ subsets: ['latin'] });
 
 export const Layout = ({ children, className, items }: LayoutProps) => {
   const [routes, setRoutes] = React.useState<BreadcrumbRoute[]>([]);
@@ -19,7 +21,9 @@ export const Layout = ({ children, className, items }: LayoutProps) => {
   };
   return (
     <BreadcrumbContext.Provider value={context}>
-      <div className="flex min-h-screen max-h-screen overflow-hidden md:flex-cols-[220px_1fr] lg:flex-cols-[280px_1fr] bg-slate-100 dark:bg-slate-800">
+      <div
+        className="flex min-h-screen max-h-screen overflow-hidden md:flex-cols-[220px_1fr] lg:flex-cols-[280px_1fr] bg-slate-100 dark:bg-slate-800"
+        style={{ fontFamily: inter.style.fontFamily }}>
         <Sidebar menuItems={items} />
         <div className="flex-1 flex flex-col overflow-hidden">
           <Header menuItems={items} />
