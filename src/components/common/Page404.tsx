88b786879815e53df14f1e5668101e50afcebcb1
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2024-11-28 10:14:31 +0100

    fix(Page404, page/404) - repair page 404 by using nextjs best practices to reimplement it

--- src/components/common/Page404.tsx
+++ src/components/common/Page404.tsx
@@ -10,7 +10,7 @@ interface Page404Props {
 export const Page404 = ({ className }: Page404Props) => {
   const { t } = useTranslation('common');
   return (
-    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div className={cn('py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6', className)}>
       <div className="mx-auto max-w-screen-sm text-center">
         <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
           404
