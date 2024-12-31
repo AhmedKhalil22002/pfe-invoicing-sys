0c91738fb678e042ba0b5dd7ded7166accf83838
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2024-12-31 12:01:21 +0100

    feat(UserDropdown) - add user dropdown component

---
+++ src/components/layout/UserDropdown.tsx
@@ -0,0 +1,41 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu';
+import { Button } from '../ui/button';
+import { CircleUser, LogOut, Settings } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+interface UserDropdownProps {
+  className?: string;
+}
+
+export const UserDropdown = ({ className }: UserDropdownProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" size="icon" className={cn('rounded-full', className)}>
+          <CircleUser className="h-7 w-7" />
+          <span className="sr-only">Toggle user menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="flex items-center gap-2">
+          <Settings className="h-5 w-5" />
+          Settings
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="flex items-center gap-2">
+          <LogOut className="h-5 w-5" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
